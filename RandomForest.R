e9479f71bed1e44bd3fb5b9fd2a5048b744a609c
Davis Dowdle <davis.dowdle@gmail.com>
2023-12-10 20:10:48 -0700

    Initialize trainging/testing data and random forest file

---
+++ RandomForest.R
@@ -0,0 +1,6 @@
+library(vroom)
+
+setwd("C:/Users/davis/OneDrive - Brigham Young University/Documents/skool/new/stat 348/AllstateClaims/AllstateClaims")
+
+train <- vroom("train.csv")
+test <- vroom("test.csv")
